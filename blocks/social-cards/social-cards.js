412585bd8631c793324d3ff51c5c863938ad3c18
Tarun Raja <tarun@Taruns-MacBook-Air.local>
2025-06-07 19:32:30 +1000

    social cards

--- blocks/social-cards/social-cards.js
+++ blocks/social-cards/social-cards.js
@@ -7,9 +7,29 @@ export default function decorate(block) {
     const li = document.createElement('li');
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'social-cards-card-image';
-      else div.className = 'social-cards-card-body';
+      if (div.children.length === 1 && div.querySelector('picture')) {
+        div.className = 'social-cards-card-image';
+      } else {
+        div.className = 'social-cards-card-body';
+
+        // Wrap non-button <p> tags in a .card-text container
+        const paragraphs = [...div.querySelectorAll('p')];
+        const textWrapper = document.createElement('div');
+        textWrapper.className = 'social-card-text';
+
+        paragraphs.forEach((p) => {
+          if (!p.classList.contains('button-container')) {
+            textWrapper.appendChild(p);
+          }
+        });
+
+        // Only insert if there are paragraphs to wrap
+        if (textWrapper.children.length > 0) {
+          div.insertBefore(textWrapper, div.firstChild);
+        }
+      }
     });
+
     ul.append(li);
   });
   ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
